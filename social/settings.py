2e783ff85f464d6e5993ea11d5d35a06d3518b5c
Alex <alextvrus@gmail.com>
2022-10-18 21:57:22 +0300

    Add Authentication.

--- social/settings.py
+++ social/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dwitter",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "dashboard"
+LOGOUT_REDIRECT_URL = "dashboard"
