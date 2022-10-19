2d31b367ebf6b63c1fa52155cd469c6278fd3165
Alex <alextvrus@gmail.com>
2022-10-19 13:50:24 +0300

    Add dweets forms.

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
-
-
-def dashboard(request):
-    return render(request, "base.html")
+from .forms import DweetForm
 
 
 def profile_list(request):
@@ -29,4 +26,11 @@ def profile(request, pk):
 
 
 def dashboard(request):
-    return render(request, "dwitter/dashboard.html")
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+    return render(request, "dwitter/dashboard.html", {"form": form})
